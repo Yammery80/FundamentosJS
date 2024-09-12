5a0087fb01330299f623de80e7de878c91ed3e02
Suzukusan93 <meribethperez27@gmail.com>
2024-09-12 14:09:22 -0600

    0.3 Variables

---
+++ concepts/operator.js
@@ -0,0 +1,33 @@
+console.log(2+1);
+console.log(2-1);
+console.log(2*1);
+console.log(2/1);
+console.log(2%1);
+console.log(2**1);
+
+//Operadores de asignación
+let a= 10;
+a += 5;
+a -= 5;
+a *= 5;
+a /= 5;
+a %= 5;
+a ** 5;
+console.log(a);
+
+//Operaciones de comparación
+console.log(2>2);
+console.log(2<2);
+console.log(2>=2);
+console.log(2<=2);
+console.log(2===2);
+console.log(2==2);
+console.log(2!=2);
+console.log(2!==2);
+
+//Operadores lógicos
+console.log(true && true); //and
+console.log(true || false); //or
+console.log(!true); //not
+
+
---
+++ concepts/variables.js
@@ -0,0 +1,21 @@
+// Declaración de variables con let y const
+//var: en desuso
+
+//variables let: cambiarán de valor 
+let age; //declaración
+console.log(age);
+age=23; //Inicialización
+console.log(age);
+
+let name= "Francisco"; //declaración e inicialización
+console.log(name);
+name ="Paco";
+console.log(name);
+
+//Constantes const: no cambiarán de valor
+const PI = 3.1416;
+PI = 3.1416;
+PI = 3.15;
+console.log(PI);
+
+
