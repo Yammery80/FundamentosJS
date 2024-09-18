e8bef18b1d053c384dc7f58454e449b5c9752f34
Suzukusan93 <meribethperez27@gmail.com>
2024-09-18 13:39:04 -0600

    02 Operadores

---
+++ concepts/conditionals.js
@@ -0,0 +1,12 @@
+// if else if else
+
+let age = 18;
+if(age >= 18){
+    console.log("¡Es Mayor de Edad: ");
+}else if(age > 15){
+    console.log("¡Es un joven!");
+}else if (age > 5){
+    console.log("¡Es un niño!");
+}else{
+    console.log("¡Es un bebé!")
+}
