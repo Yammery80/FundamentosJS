df9b157aa0a707b5756200fdee19f84ffcd53a3c
Suzukusan93 <meribethperez27@gmail.com>
2024-09-18 13:41:45 -0600

    01 Tipos de Datos

--- concepts/conditionals.js
+++ concepts/conditionals.js
@@ -5,8 +5,6 @@ if(age >= 18){
     console.log("¡Es Mayor de Edad: ");
 }else if(age > 15){
     console.log("¡Es un joven!");
-}else if (age > 5){
-    console.log("¡Es un niño!");
 }else{
     console.log("¡Es un bebé!")
 }
